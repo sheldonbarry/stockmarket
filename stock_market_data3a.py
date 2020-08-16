e2d51a8882752af1d9e6ec7908fc49c5cd59fc99
Sheldon Barry <sheldonbarry@users.noreply.github.com>
2020-08-16 12:47:36 +1000

    Update stock_market_data3a.py
    Fixed print format syntax

--- stock_market_data3a.py
+++ stock_market_data3a.py
@@ -12,7 +12,7 @@ pb = ticker_stats['priceToBook'].values[0]
 dy = ticker_stats['trailingAnnualDividendYield'].values[0]
 
 print('Ticker\t\tP/E\t\tP/B\t\tDivY')
-print('{}\t\t{:.2f}\t{:.4f}\t{:.4f}').format(symbol, pe, pb, dy)
+print('{}\t\t{:.2f}\t{:.4f}\t{:.4f}'.format(symbol, pe, pb, dy))
 
 # to see a list of all the column names, issue the command:
 # print ticker_stats.columns
